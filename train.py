5263882852fe95ea20a79b0f895011d57403b999
ChintanTrivedi <abt53@scarletmail.rutgers.edu>
2019-07-25 18:03:47 +0530

    updated training models

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 import gfootball.env as football_env
 import numpy as np
 
+import tensorflow as tf
+from keras.callbacks import TensorBoard
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten
 from keras.models import Model
 from keras.optimizers import Adam
 from keras import backend as K
+from keras.applications.mobilenet_v2 import MobileNetV2
 
 clipping_val = 0.2
 critic_discount = 0.5
@@ -22,61 +25,53 @@ def get_advantages(values, masks, rewards):
         returns.insert(0, gae + values[i])
 
     adv = np.array(returns) - values[:-1]
-    return (adv - np.mean(adv)) / (np.std(adv) + 1e-8)
+    return returns, (adv - np.mean(adv)) / (np.std(adv) + 1e-8)
 
 
 def ppo_loss(oldpolicy_probs, advantages, rewards, values):
     def loss(y_true, y_pred):
-        newpolicy_probs = y_true * y_pred
-        ratio = K.exp(K.log(newpolicy_probs) - K.log(oldpolicy_probs))
+        y_true = tf.Print(y_true, [y_true], 'y_true: ')
+        y_pred = tf.Print(y_pred, [y_pred], 'y_pred: ')
+        newpolicy_probs = y_pred
+        # newpolicy_probs = y_true * y_pred
+        newpolicy_probs = tf.Print(newpolicy_probs, [newpolicy_probs], 'new policy probs: ')
+
+        ratio = K.exp(K.log(newpolicy_probs + 1e-10) - K.log(oldpolicy_probs + 1e-10))
+        ratio = tf.Print(ratio, [ratio], 'ratio: ')
         p1 = ratio * advantages
         p2 = K.clip(ratio, min_value=1 - clipping_val, max_value=1 + clipping_val) * advantages
         actor_loss = -K.mean(K.minimum(p1, p2))
+        actor_loss = tf.Print(actor_loss, [actor_loss], 'actor_loss: ')
         critic_loss = K.mean(K.square(rewards - values))
-        total_loss = critic_discount * critic_loss + actor_loss - entropy_beta * K.mean(-(
-                newpolicy_probs * K.log(newpolicy_probs)))
+        critic_loss = tf.Print(critic_loss, [critic_loss], 'critic_loss: ')
+        term_a = critic_discount * critic_loss
+        term_a = tf.Print(term_a, [term_a], 'term_a: ')
+        term_b_2 = K.log(newpolicy_probs + 1e-10)
+        term_b_2 = tf.Print(term_b_2, [term_b_2], 'term_b_2: ')
+        term_b = entropy_beta * K.mean(-(newpolicy_probs * term_b_2))
+        term_b = tf.Print(term_b, [term_b], 'term_b: ')
+        total_loss = term_a + actor_loss - term_b
+        total_loss = tf.Print(total_loss, [total_loss], 'total_loss: ')
         return total_loss
 
     return loss
 
 
-def get_model_actor(input_dims, output_dims):
+def get_model_actor_pretrained(input_dims, output_dims):
     state_input = Input(shape=input_dims)
     oldpolicy_probs = Input(shape=(1, output_dims,))
     advantages = Input(shape=(1, 1,))
     rewards = Input(shape=(1, 1,))
     values = Input(shape=(1, 1,))
 
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(state_input)
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    feature_extractor = MobileNetV2(include_top=False, weights='imagenet')
 
-    # Block 2
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
-
-    # Block 3
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
-    # Block 4
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
-
-    # Block 5
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+    for layer in feature_extractor.layers:
+        layer.trainable = False
 
     # Classification block
-    x = Flatten(name='flatten')(x)
-    x = Dense(4096, activation='relu', name='fc1')(x)
-    x = Dense(4096, activation='relu', name='fc2')(x)
+    x = Flatten(name='flatten')(feature_extractor(state_input))
+    x = Dense(1024, activation='relu', name='fc1')(x)
     out_actions = Dense(n_actions, activation='softmax', name='predictions')(x)
 
     model = Model(inputs=[state_input, oldpolicy_probs, advantages, rewards, values],
@@ -86,46 +81,26 @@ def get_model_actor(input_dims, output_dims):
         advantages=advantages,
         rewards=rewards,
         values=values)])
+    model.summary()
     return model
 
 
-def get_model_critic(input_dims):
+def get_model_critic_pretrained(input_dims):
     state_input = Input(shape=input_dims)
 
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(state_input)
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    feature_extractor = MobileNetV2(include_top=False, weights='imagenet')
 
-    # Block 2
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
-
-    # Block 3
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
-    # Block 4
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
-
-    # Block 5
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+    for layer in feature_extractor.layers:
+        layer.trainable = False
 
     # Classification block
-    x = Flatten(name='flatten')(x)
-    x = Dense(4096, activation='relu', name='fc1')(x)
-    x = Dense(4096, activation='relu', name='fc2')(x)
+    x = Flatten(name='flatten')(feature_extractor(state_input))
+    x = Dense(1024, activation='relu', name='fc1')(x)
     out_actions = Dense(1, activation='tanh')(x)
 
     model = Model(inputs=[state_input], outputs=[out_actions])
     model.compile(optimizer=Adam(lr=1e-4), loss='mse')
+    model.summary()
     return model
 
 
@@ -143,6 +118,12 @@ def test_reward():
     return total_reward
 
 
+def one_hot_encoding(probs):
+    one_hot = np.zeros_like(probs)
+    one_hot[:, np.argmax(probs, axis=1)] = 1
+    return one_hot
+
+
 env = football_env.create_environment(
     env_name='academy_empty_goal_close', representation='pixels', render=True)
 
@@ -153,8 +134,10 @@ n_actions = env.action_space.n
 dummy_n = np.zeros((1, 1, n_actions))
 dummy_1 = np.zeros((1, 1, 1))
 
-model_actor = get_model_actor(input_dims=state_dims, output_dims=n_actions)
-model_critic = get_model_critic(input_dims=state_dims)
+tensor_board = TensorBoard(log_dir='./logs')
+
+model_actor = get_model_actor_pretrained(input_dims=state_dims, output_dims=n_actions)
+model_critic = get_model_critic_pretrained(input_dims=state_dims)
 
 ppo_steps = 128
 target_reached = False
@@ -162,7 +145,7 @@ best_reward = 0
 iters = 0
 max_iters = 20
 
-while not target_reached:
+while not target_reached and iters < max_iters:
 
     states = []
     actions = []
@@ -172,12 +155,12 @@ while not target_reached:
     action_probs = []
     state_input = None
 
-    for _ in range(ppo_steps):
+    for itr in range(ppo_steps):
         state_input = K.expand_dims(state, 0)
         action_dist = model_actor.predict([state_input, dummy_n, dummy_1, dummy_1, dummy_1], steps=1)
-        print(np.argmax(action_dist))
         q_value = model_critic.predict([state_input], steps=1)
         action = np.argmax(action_dist)
+        print(str(itr) + ':' + str(action))
         # action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
         mask = not done
@@ -194,13 +177,16 @@ while not target_reached:
 
     q_value = model_critic.predict(state_input, steps=1)
     values.append(q_value)
-
-    advantages = get_advantages(values, masks, rewards)
-    model_actor.fit([states, action_probs, advantages, np.reshape(rewards, newshape=(-1, 1, 1)), values[:-1]],
-                    [np.reshape(action_probs, newshape=(-1, n_actions))], verbose=True)
-    model_critic.fit([states], [rewards], shuffle=True, epochs=8, verbose=True)
+    returns, advantages = get_advantages(values, masks, rewards)
+    actor_loss = model_actor.fit(
+        [states, action_probs, advantages, np.reshape(rewards, newshape=(-1, 1, 1)), values[:-1]],
+        [one_hot_encoding(np.reshape(action_probs, newshape=(-1, n_actions)))], verbose=True, shuffle=True, epochs=8,
+        callbacks=[tensor_board])
+    critic_loss = model_critic.fit([states], [np.reshape(returns, newshape=(-1, 1))], shuffle=True, epochs=8,
+                                   verbose=True, callbacks=[tensor_board])
 
     avg_reward = np.mean([test_reward() for _ in range(10)])
+    print('reward=' + str(avg_reward))
     if avg_reward > best_reward:
         print('best reward=' + str(avg_reward))
         model_actor.save('model_actor_{}_{}.hdf5'.format(iters, avg_reward))
@@ -209,5 +195,6 @@ while not target_reached:
     if best_reward > 0.9 or iters > max_iters:
         target_reached = True
     iters += 1
+    env.reset()
 
 env.close()
